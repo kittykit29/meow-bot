98cb5c70040c1d137bfa22c88912eb6d497a9abc
kittykit29 <qiratriaz0@gmail.com>
2026-08-13 16:56:32 +0500

    added hugall command

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions
     ]
 });
 
